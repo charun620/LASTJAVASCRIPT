9f238f300121651538c94560016e5ea117f5211e
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-07-18 23:15:40 +0900

    c

--- Lec3/ex5.js
+++ Lec3/ex5.js
@@ -1,4 +1,16 @@
 function CountposMisum(arr) {
-    
+  let pos = 0;
+  let mis = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] >= 0) {
+      pos++;
+    }
+    if (arr[index] < 0) {
+      mis = mis + arr[index];
+    }
+  }
+  return [pos,mis];
 }
-console.log(CountposMisum[1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]);
+console.log(
+  CountposMisum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+);
