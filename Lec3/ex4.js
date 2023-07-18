9df2286c43d621df2ca20d615471a064b952bbf1
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-07-18 19:39:31 +0700

    commit

--- Lec3/ex4.js
+++ Lec3/ex4.js
@@ -1,4 +1,13 @@
 function numsum(arr) {
+    let num=0;
+    for (let index = 0; index < arr.length; index++) {
+        if (typeof arr[index]==num) {
+            console.log(typeof arr[index]);
+            num =num+arr[index]
+        }
+        
+    }
+    return num;
     
 }
 console.log(numsum([1,2,"13","4","165"]));
