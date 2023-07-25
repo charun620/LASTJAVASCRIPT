750c9bb56bbadf4c76ca98dcafe089d644f7021c
401A-26\CHARN <s6206021622178@fitm.kmutnb.ac.th>
2023-07-25 08:24:31 +0700

    create

---
+++ Lec4/construc.js
@@ -0,0 +1,11 @@
+class Song {
+    constructor(title, artist) {
+      this.title = title;
+      this.artist = artist;
+    }
+  }
+  
+  const mySong = new Song("mycraft", "mojang");
+  console.log(mySong.title);
+  console.log(mySong.artist);
+  
---
+++ Lec4/stati.js
@@ -0,0 +1,20 @@
+class Dog {
+    constructor(name) {
+      this._name = name;
+    }
+  
+    introduce() {
+      console.log("This is " + this._name + " !");
+    }
+  
+    static bark() {
+      // static ใช้ได้เฉพาะ ตัวเจ้าของ class เท่านั้น มีไว้ใช้จัดการคลาส
+      console.log("Woof!");
+    }
+  }
+  
+  const myDog = new Dog("scooter");
+  myDog.introduce();
+  
+  Dog.bark(); // ตัวอย่าง
+  
