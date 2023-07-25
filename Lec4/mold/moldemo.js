3396240efa9c9bd39f6725d8469f68e2305985f2
401A-26\CHARN <s6206021622178@fitm.kmutnb.ac.th>
2023-07-25 08:47:29 +0700

    create

---
+++ Lec4/mold/moldemo.js
@@ -0,0 +1,20 @@
+//libhrllo.js
+
+const person = {
+    name: "Panwan",
+    weight: 85,
+  };
+  
+  function sayHello() {}
+  console.log("Hello World!");
+  
+  function cube(x) {
+    return x * x * x;
+  }
+  
+  const add = (a, b) => a + b;
+  
+  const status = true;
+  
+  module.exports = { person, sayHello, cube, add, status };
+  
