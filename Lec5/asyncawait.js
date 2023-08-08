33aace8833bbf92242dc483ddbb57fe43802e752
401A-26\CHARN <s6206021622178@fitm.kmutnb.ac.th>
2023-08-08 08:40:31 +0700

    modified:   Lec5/asyncawait.js

--- Lec5/asyncawait.js
+++ Lec5/asyncawait.js
@@ -6,6 +6,7 @@ function promiseTimeout(ms) {
   
   async function run() {
     console.log("Start!!");
+    //try take of await and compare
     await promiseTimeout(2000);
     console.log("Stop!!");
   }
