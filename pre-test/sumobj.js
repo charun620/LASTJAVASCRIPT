e35405ed0c09d41e770d2befb8b37dfd669e6527
CHRNPS <51948655+charun620@users.noreply.github.com>
2023-08-28 23:01:26 +0900

    Update sumobj.js

--- pre-test/sumobj.js
+++ pre-test/sumobj.js
@@ -1,3 +1,28 @@
 function sumobj(obj) {
-    
+    function getBudgets(people) {
+  let totalBudget = 0;
+
+  for (let person of people) {
+    totalBudget += person.budget;
+  }
+
+  return totalBudget;
+}
+
+// Test cases
+const people1 = [
+  { name: "John", age: 21, budget: 23000 },
+  { name: "Steve", age: 32, budget: 40000 },
+  { name: "Martin", age: 16, budget: 2700 }
+];
+
+const people2 = [
+  { name: "John", age: 21, budget: 29000 },
+  { name: "Steve", age: 32, budget: 32000 },
+  { name: "Martin", age: 16, budget: 1600 }
+];
+
+console.log(getBudgets(people1)); // Output: 65700
+console.log(getBudgets(people2)); // Output: 62600
+
 }
